1f7f970d7bc4abff571b3b89fe7243914135a340
maryamzain123 <smaryam.zain@gmail.com>
2021-03-12 19:04:28 -0600

    commit tree 0159ac3af557b6d5bf354f0a664101882a156e1d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3, :all_sides
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @all_sides = [@side_1, @side_2, @side_3].sort
+  end
+
+  def kind 
+    if real_triangle
+      begin
+        raise TriangleError
+      end
+    end
+
+    if side_1 == side_2 && side_2 == side_3
+      return :equilateral
+    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def real_triangle
+    all_sides.any? do |side|
+      side < 0 || all_sides[0] + all_sides[1] <= all_sides[2]
+    end
+  end
+
+
+  class TriangleError < StandardError
+  end
 end
